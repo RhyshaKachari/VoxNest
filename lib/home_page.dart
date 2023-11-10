9b1655d84d7f69a29a837133590d76a8390a3a6e
Rhysha Kachari <rhyshakachari2003@gmail.com>
2023-11-10 11:26:51 +0530

    created a chat bubble

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -42,7 +42,35 @@ class _HomePageState extends State<HomePage> {
               ),
             ],
           ),
-
+          //chat bubble code
+          Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 10,
+            ),
+            margin: const EdgeInsets.symmetric(horizontal: 40).copyWith(
+              top: 30,
+            ),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Pallete.borderColor,
+              ),
+              borderRadius: BorderRadius.circular(20).copyWith(
+                topLeft: Radius.zero,
+              ),
+            ),
+            child: const Padding(padding: EdgeInsets.symmetric(
+              vertical: 10.0
+            ),
+            child: Text(
+              'Good Morning , what task can I do for you?',
+              style: TextStyle(
+                fontFamily: 'Cera Pro',
+                color: Pallete.mainFontColor,
+                fontSize: 20,
+              ),
+            ),),
+          ),
         ],
       )
     );
