b11223566a153f5ba696e7faec1907abe861c219
Rhysha Kachari <rhyshakachari2003@gmail.com>
2023-11-10 12:17:29 +0530

    added mic button and suggestion feature boxes

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'pallete.dart';
+import 'feature_box.dart';
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -16,79 +17,107 @@ class _HomePageState extends State<HomePage> {
         leading: const Icon(Icons.menu),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          Stack(
-            children: [
-              Center(
-                child: Container(
-                  height: 120,
-                  width: 120,
-                  margin: const EdgeInsets.only(top: 4),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Center(
+                  child: Container(
+                    height: 120,
+                    width: 120,
+                    margin: const EdgeInsets.only(top: 4),
+                    decoration: const BoxDecoration(
+                      color: Pallete.assistantCircleColor,
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 123,
                   decoration: const BoxDecoration(
-                    color: Pallete.assistantCircleColor,
                     shape: BoxShape.circle,
+                    image: DecorationImage(image: AssetImage('assets/images/virtualAssistant.png',
+                    ),
+                    ),
                   ),
                 ),
+              ],
+            ),
+            //chat bubble code
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 10,
               ),
-              Container(
-                height: 123,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(image: AssetImage('assets/images/virtualAssistant.png',
-                  ),
-                  ),
+              margin: const EdgeInsets.symmetric(horizontal: 40).copyWith(
+                top: 30,
+              ),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Pallete.borderColor,
+                ),
+                borderRadius: BorderRadius.circular(20).copyWith(
+                  topLeft: Radius.zero,
                 ),
               ),
-            ],
-          ),
-          //chat bubble code
-          Container(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 20,
-              vertical: 10,
-            ),
-            margin: const EdgeInsets.symmetric(horizontal: 40).copyWith(
-              top: 30,
-            ),
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Pallete.borderColor,
+              child: const Padding(padding: EdgeInsets.symmetric(
+                vertical: 10.0
               ),
-              borderRadius: BorderRadius.circular(20).copyWith(
-                topLeft: Radius.zero,
-              ),
+              child: Text(
+                'Good Morning , what task can I do for you?',
+                style: TextStyle(
+                  fontFamily: 'Cera Pro',
+                  color: Pallete.mainFontColor,
+                  fontSize: 20,
+                ),
+              ),),
             ),
-            child: const Padding(padding: EdgeInsets.symmetric(
-              vertical: 10.0
-            ),
-            child: Text(
-              'Good Morning , what task can I do for you?',
-              style: TextStyle(
-                fontFamily: 'Cera Pro',
-                color: Pallete.mainFontColor,
-                fontSize: 20,
-              ),
-            ),),
-          ),
-          //little text
-          Container(
-            padding: const EdgeInsets.all(10),
-            alignment: Alignment.centerLeft,
-            margin: const EdgeInsets.only(top: 10 , left: 22),
-            child: const Text(
-              'Here are a few features',
-              style: TextStyle(
-                fontFamily: 'Cera Pro',
-                color: Pallete.mainFontColor,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+            //little text
+            Container(
+              padding: const EdgeInsets.all(10),
+              alignment: Alignment.centerLeft,
+              margin: const EdgeInsets.only(top: 10 , left: 22),
+              child: const Text(
+                'Here are a few features',
+                style: TextStyle(
+                  fontFamily: 'Cera Pro',
+                  color: Pallete.mainFontColor,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          )
-          //suggestions List
-        ],
-      )
+            //features List
+           Column(
+             children: const [
+               FeatureBox(
+                  color: Pallete.firstSuggestionBoxColor,
+                 headerText: 'ChatGPT',
+                 descriptionText: 'A smarter way to stay organized and informed with ChatGPT',
+               ),
+               FeatureBox(
+                 color: Pallete.secondSuggestionBoxColor,
+                 headerText: 'Dall-E',
+                 descriptionText: 'Get inspired and stay creative with your personal assistant powered by Dall-E',
+               ),
+               FeatureBox(
+                 color: Pallete.thirdSuggestionBoxColor,
+                 headerText: 'Smart Voice Assistant',
+                 descriptionText: 'Get the best of both worlds with a voice assistant powered by Dall-E and ChatGPT',
+               ),
+             ],
+
+           )
+          ],
+        ),
+
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Pallete.secondSuggestionBoxColor,
+          onPressed: (){},
+        child: const Icon(Icons.mic),
+      ) ,
     );
   }
 }
